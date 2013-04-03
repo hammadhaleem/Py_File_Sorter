d42b646fd68d59675145a2755ef4e56358a42377
hammad haleem <hammadhaleem@gmail.com>
2013-04-03 12:59:36 +0530

    Added Basic Structure

---
+++ sort.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+import os
+import sys
+import getpass
+import shutil
+
+_user = getpass.getuser()
+dir="/home/"+_user+"/Downloads/"
+
+def sort(f_type):
+  try:  
+      dirname=f_type.upper()
+      os.chdir(dir)
+      for files in os.listdir("."):
+  
+	  if files.endswith(f_type):
+	
+	      if os.path.isdir(dir+dirname):
+		  shutil.move(dir+files,dir+dirname)
+        
+	      else:
+		  os.mkdir(dir+dirname)
+		  shutil.move(dir+files,dir+dirname)
+		
+      return	  
+  
+  except IOError:
+      print "Error in reading file"
+# ADD MORE EXTENSIONS HERE IF YOU WANT
+
+
+sort("mp3")
+sort("rpm")
+sort("zip")
+sort("pdf")
+sort("doc")
+sort("jpg")
+sort("flv")
+sort("tar")
+sort("torrent")
+sort("txt")
+sort("zip")
+sort("exe")
+#sort("")
